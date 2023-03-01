d409569edd512c759a9b07cb851a29dab3c2cca7
Aditya Rai <rai.aditya01@gmail.com>
2023-03-01 12:50:47 +0530

    unit_conv function added

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="ExoPSI",
-    version="0.1.2",
+    version="0.1.4",
     description="Library to calculate Similarity Indexes",
     long_description=long_description,
     long_description_content_type="text/markdown",
